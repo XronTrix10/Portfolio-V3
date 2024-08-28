aa97886d72dd34a5b3f7529df3dac9132c9184fc
Xron Trix <xrontrix@gmail.com>
2024-08-28 14:22:51 +0530

    feat: animated custom home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import type { FC } from "react";
 
+import { TextAnimate } from "@/components/ui/text-animate";
+import "@/styles/hero.css";
 /**
  * renders the home page.
  * @returns {FC} the home page.
@@ -7,7 +9,23 @@ import type { FC } from "react";
 const Home: FC = () => {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-6xl font-semibold md:text-9xl">Hey!</h1>
+      <div className="flex flex-col justify-center gap-4 text-left">
+        <h2 className="text-4xl">
+          <span className="greet">Hey!</span>{" "}
+          <span className="identity">I&apos;m</span>
+        </h2>
+
+        <TextAnimate
+          text="Xron Trix"
+          type="fadeIn"
+          className="name-color mb-4 font-limelight text-[6rem] leading-none"
+        />
+        {/* <h1 className="name">Xron Trix</h1> */}
+
+        <button className="w-fit border border-dark-gray bg-transparent px-3 py-2 shadow-xl transition-all duration-700 hover:bg-light hover:text-black hover:shadow-black">
+          Jump -&gt;
+        </button>
+      </div>
     </main>
   );
 };
