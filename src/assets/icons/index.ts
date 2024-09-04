1eb423e53fceee681f6658a7f2a49f1f6b5be228
Xron Trix <xrontrix@gmail.com>
2024-09-05 01:09:20 +0530

    feat: added custom svg icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as HomeIcon } from "./home";
+export { default as Project } from "./project";
+export { default as WorkIcon } from "./work";
+export { default as SendMessageIcon } from "./send-message";
+export { default as UserPersonalIcon } from "./user-personal";
