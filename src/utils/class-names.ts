a616f3af00edf5ee878dd4705f999f0a5dd3dd8f
Xron Trix <xrontrix@gmail.com>
2024-09-09 21:44:17 +0530

    feat: added classname merge function for tailwind

--- src/utils/class-names.ts
+++ src/utils/class-names.ts
@@ -1,3 +1,6 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 /**
  * joins classes together.
  * @param {string[]} classes the classes to join.
@@ -5,5 +8,14 @@
  */
 const classNames = (...classes: string[]): string =>
   classes.filter(Boolean).join(" ");
+
+/**
+ * joins tailwind classes together.
+ * @param {ClassValue[]} inputs the tailwind classes to join.
+ * @returns {string} the joined tailwind classes string.
+ */
+export const cn = (...inputs: ClassValue[]): string => {
+  return twMerge(clsx(inputs));
+};
 
 export default classNames;
