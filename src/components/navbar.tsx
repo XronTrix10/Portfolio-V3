e60fa73f0258bd6f8539ac8f3a954dda8948d0bc
Xron Trix <xrontrix@gmail.com>
2024-09-09 10:11:59 +0530

    feat: new button in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,6 +5,7 @@ import {
   HomeIcon,
   Project,
   SendMessageIcon,
+  Terminal as TerminalIcon,
   UserPersonalIcon,
   WorkIcon,
 } from "@/assets/icons";
@@ -19,22 +20,30 @@ const links =
 const Navbar = (): JSX.Element => {
   return (
     <nav className="fixed bottom-6 z-20 w-screen">
-      <div className="mx-auto flex w-fit items-center justify-between gap-x-2 rounded-full border border-dark-gray/50 bg-light/5 px-3 py-2 text-sm text-light-gray/80 shadow-xl shadow-black *:text-xs">
-        <Link href="/overview" className={links}>
-          <HomeIcon />
-        </Link>
-        <Link href="/projects" className={links}>
-          <Project />
-        </Link>
-        <Link href="/experience" className={links}>
-          <WorkIcon />
-        </Link>
-        <Link href="/contact" className={links}>
-          <SendMessageIcon />
-        </Link>
-        <Link href="/personal" className={links}>
-          <UserPersonalIcon />
-        </Link>
+      <div className="mx-auto flex w-fit items-center gap-x-4">
+        <div className="flex w-fit items-center justify-between gap-x-2 rounded-full border border-dark-gray/50 bg-light/5 px-2 py-1 text-sm text-light-gray/80 shadow-xl shadow-black *:text-xs 2xl:px-3 2xl:py-2">
+          <Link href="/overview" className={links}>
+            <HomeIcon />
+          </Link>
+          <Link href="/projects" className={links}>
+            <Project />
+          </Link>
+          <Link href="/experience" className={links}>
+            <WorkIcon />
+          </Link>
+          <Link href="/contact" className={links}>
+            <SendMessageIcon />
+          </Link>
+          <Link href="/personal" className={links}>
+            <UserPersonalIcon />
+          </Link>
+        </div>
+
+        <div className="flex w-fit items-center justify-between gap-x-2 rounded-full border border-dark-gray/50 bg-light/5 px-2 py-1 text-sm text-light-gray/80 shadow-xl shadow-black *:text-xs 2xl:px-3 2xl:py-2">
+          <button className="p-2 transition-all duration-500 hover:text-light">
+            <TerminalIcon />
+          </button>
+        </div>
       </div>
     </nav>
   );
