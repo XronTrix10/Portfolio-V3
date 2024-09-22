9430e164cca7ebf87fa12206127c4ece746ee4a8
Xron Trix <xrontrix@gmail.com>
2024-09-22 17:23:47 +0530

    feat: dynamic rendering by fetchng count value

--- src/app/(navgroup)/layout.tsx
+++ src/app/(navgroup)/layout.tsx
@@ -8,19 +8,36 @@ type NavLayoutProps = {
 };
 
 /**
- * renders the layout component.
+ * Renders the navbar group layout.
+ * It fetches the count from the database and passes it to the footer component.
  * @param {NavLayoutProps} props the layout component props.
  * @returns {JSX.Element} the layout component.
  */
-const NavLayout: FC<NavLayoutProps> = ({
+const NavLayout: FC<NavLayoutProps> = async ({
   children,
-}: NavLayoutProps): JSX.Element => {
+}: NavLayoutProps): Promise<JSX.Element> => {
+  let count = 1;
+
+  try {
+    const res = await fetch(new URL("/api/db", process.env.BASE_ORIGIN), {
+      method: "GET",
+      cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const responseJson: { count: number } = await res.json();
+    count = responseJson.count;
+  } catch (error) {
+    console.error(error);
+  }
+
   return (
     <div>
       <Navbar />
       <div className="mx-auto max-w-screen-lg 2xl:max-w-screen-xl">
         {children}
-        <Footer count={78} />
+        <Footer count={count} />
       </div>
     </div>
   );
