b559288c0430ba9abcd0934124af4bb4417c80b8
Xron Trix <xrontrix@gmail.com>
2024-09-23 23:25:50 +0530

    feat: added meta tags for link preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import * as React from "react";
 import type { JSX } from "react";
 import { Inter, JetBrains_Mono, Limelight } from "next/font/google";
@@ -44,6 +45,31 @@ export default function RootLayout({
 }>): JSX.Element {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:title" content="Xron Trix" />
+        <meta
+          property="og:description"
+          content="The portfolio website of Xron Trix"
+        />
+        <meta
+          property="og:image"
+          content="https://gist.github.com/user-attachments/assets/57338740-e086-4af1-bf8f-d94d66549de7"
+        />
+        <meta property="og:url" content="https://xrontrix.pages.dev" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@XronTrix10" />
+        <meta name="twitter:creator" content="@XronTrix10" />
+        <meta name="twitter:title" content="Xron Trix" />
+        <meta
+          name="twitter:description"
+          content="The portfolio website of Xron Trix"
+        />
+        <meta
+          name="twitter:image"
+          content="https://gist.github.com/user-attachments/assets/57338740-e086-4af1-bf8f-d94d66549de7"
+        />
+      </Head>
       <body
         className={classNames(
           inter.variable,
